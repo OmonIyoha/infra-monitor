5a9d5b6a2a77c0d0bcbc7f8a12ab5dc42129b88a
Omon Iyoha <Sharongoddey84@gmail.com>
2026-05-28 18:54:33 +0100

    Initial infrastructure monitoring tool

---
+++ monitor.py
@@ -0,0 +1,31 @@
+import psutil
+import time
+from datetime import datetime
+
+LOG_FILE = "logs/system.log"
+
+def write_log(message):
+    with open(LOG_FILE, "a") as f:
+        f.write(message + "\n")
+
+while True:
+
+    cpu = psutil.cpu_percent()
+    memory = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+
+    message = (
+        f"{datetime.now()} | "
+        f"CPU:{cpu}% "
+        f"MEM:{memory}% "
+        f"DISK:{disk}%"
+    )
+
+    print(message)
+
+    write_log(message)
+
+    if cpu > 80:
+        write_log("ALERT: CPU HIGH")
+
+    time.sleep(10)
